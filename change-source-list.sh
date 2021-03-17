0a05f1b817acfb379d1468280782097ff882acb7
root <root@jeeyong-asus.localdomain>
2021-03-18 01:44:38 +0900

    백업 기능 추가

--- change-source-list.sh
+++ change-source-list.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
-SOURCES_LIST=/etc/apt/sources.list
+SOURCES_LIST='/etc/apt/sources.list'
+SOURCES_LIST_BACKUP="$SOURCES_LIST.backup"
+
+# Backup sources.list
+if [ ! -f "$SOURCES_LIST_BACKUP" ]; then
+  cp "$SOURCES_LIST" "$SOURCES_LIST_BACKUP"
+fi
 
 # Repositories
 ARCHIVE='([a-z]{2}.)?archive.ubuntu.com'

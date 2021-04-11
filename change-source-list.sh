87fa5380d96094ddb1ef2b4992a009d60bb621c0
root <root@jeeyong-asus.localdomain>
2021-04-12 00:52:21 +0900

    Add comments

--- change-source-list.sh
+++ change-source-list.sh
@@ -1,20 +1,30 @@
 #!/bin/sh
 
+echo ""
+echo "############################"
+echo "# Change sources.list file #"
+echo "############################"
+
+# Backup sources.list
+echo '-> Back up "sources.list" to "sources.list.backup"'
+
 SOURCES_LIST='/etc/apt/sources.list'
 SOURCES_LIST_BACKUP="$SOURCES_LIST.backup"
 
-# Backup sources.list
 if [ ! -f "$SOURCES_LIST_BACKUP" ]; then
   cp "$SOURCES_LIST" "$SOURCES_LIST_BACKUP"
 fi
 
-# Repositories
+# Change archive repositories
+echo "-> Change archive repositories"
+
 ARCHIVE='([a-z]{2}.)?archive.ubuntu.com'
 SECURITY='security.ubuntu.com'
 KAKAO='mirror.kakao.com'
 
-# Change archive repositories
 sed -i -E "s/$ARCHIVE/$KAKAO/g" $SOURCES_LIST
 
 # Change security repositories
 sed -i "s/$SECURITY/$KAKAO/g" $SOURCES_LIST
+
+echo "-> Complete\n"

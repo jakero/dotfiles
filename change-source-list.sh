58f41eddb5a998678689696d1513c48907e8b5f1
jeeyong <jeeyong.ro@gmail.com>
2021-05-18 22:09:57 +0900

    Update script

--- change-source-list.sh
+++ change-source-list.sh
@@ -1,28 +1,28 @@
 #!/bin/sh
 
-echo "\n############################"
+echo
+echo "############################"
 echo "# Change sources.list file #"
 echo "############################"
 
 # Backup sources.list
-echo '--> Back up "sources.list" to "sources.list.backup"'
+echo '-> Back up "sources.list" to "sources.list.backup"'
 
 SOURCES_LIST='/etc/apt/sources.list'
 SOURCES_LIST_BACKUP="${SOURCES_LIST}.backup"
 
-if [ ! -f "$SOURCES_LIST_BACKUP" ]; then
-  cp "$SOURCES_LIST" "$SOURCES_LIST_BACKUP"
+if [ ! -f "${SOURCES_LIST_BACKUP}" ]; then
+  cp "${SOURCES_LIST}" "${SOURCES_LIST_BACKUP}"
 fi
 
 # Change repositories
-echo "--> Change repositories"
+echo "-> Change repositories"
 
 ARCHIVE='([a-z]{2}.)?archive.ubuntu.com'
 SECURITY='security.ubuntu.com'
 KAKAO='mirror.kakao.com'
 
-sed -i -E "s/${ARCHIVE}/${KAKAO}/g" $SOURCES_LIST
-sed -i "s/${SECURITY}/${KAKAO}/g" $SOURCES_LIST
+sed -i -E "s/${ARCHIVE}/${KAKAO}/g" ${SOURCES_LIST}
+sed -i "s/${SECURITY}/${KAKAO}/g" ${SOURCES_LIST}
 
-echo "--> Done"
-
+echo -e "Done.\n"

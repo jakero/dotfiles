8985126e81abe259cbf61545e75760ef955a8f09
jeeyong <jeeyong.ro@gmail.com>
2021-06-09 13:25:07 +0900

    Update script

--- change_source_list.sh
+++ change_source_list.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 
 echo
-echo "############################"
-echo "# Change sources.list file #"
-echo "############################"
-
-# Backup sources.list
-echo '-> Back up "sources.list" to "sources.list.backup"'
+echo '############################'
+echo '# Change sources.list file #'
+echo '############################'
 
 SOURCES_LIST='/etc/apt/sources.list'
 SOURCES_LIST_BACKUP="${SOURCES_LIST}.backup"
 
-if [ ! -f "${SOURCES_LIST_BACKUP}" ]; then
+# Backup sources.list
+if [ -f "${SOURCES_LIST_BACKUP}" ]; then
+  echo "-> Backup: 'sources.list.backup' is exist."
+else
+  echo "-> Backup: 'sources.list' to 'sources.list.backup'."
+
   cp "${SOURCES_LIST}" "${SOURCES_LIST_BACKUP}"
 fi
 
 # Change repositories
-echo "-> Change repositories"
+echo '-> Change repositories'
 
 ARCHIVE='([a-z]{2}.)?archive.ubuntu.com'
 SECURITY='security.ubuntu.com'
@@ -25,4 +27,4 @@ KAKAO='mirror.kakao.com'
 sed -i -E "s/${ARCHIVE}/${KAKAO}/g" ${SOURCES_LIST}
 sed -i "s/${SECURITY}/${KAKAO}/g" ${SOURCES_LIST}
 
-echo -e "Done.\n"
+echo "-> Done.\n"
